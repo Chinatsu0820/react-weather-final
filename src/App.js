53fc1c2a396e3a8ad3dc1650c76261bfb3d612a4
Alice Lazzari <alicelazzari.al@gmail.com>
2023-08-30 00:04:45 -0300

    install-webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World..!</h1>
+      <div><p>lorem ipsum</p></div>
+    </div>
+  );
+}
+export default App;
